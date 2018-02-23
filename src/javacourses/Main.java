1dd785b47a7a3652599de28cfb908b2201f95c67
Pauline <dyakovapolina@gmail.com>
2018-02-23 12:01:17 +0300

    Asking question and repeating game

--- src/javacourses/Main.java
+++ src/javacourses/Main.java
@@ -1,7 +1,5 @@
 package javacourses;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,28 +8,38 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        String answer;
+        do {
 
-        System.out.println("I think of number from 1 to 100. Try to guess it.");
+            System.out.println("I think of number from 1 to 100. Try to guess it.");
 
-        int myNum = random.nextInt(100) + 1;
+            int myNum = random.nextInt(100) + 1;
 
-        System.out.println(myNum);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("My try is " + (i + 1) );
-            int userNum = scanner.nextInt();
+            System.out.println(myNum);
+            for (int i = 0; i < 10; i++) {
+                System.out.println("My try is " + (i + 1));
+                int userNum = scanner.nextInt();
 
-            System.out.println(userNum);
+                System.out.println(userNum);
 
-            if (myNum > userNum) {
-                System.out.println("My number is more ");
-            } else if (myNum < userNum) {
-                System.out.println("My number is less");
-            } else {
-                System.out.println("Bingo!");
-                break;
+                if (myNum > userNum) {
+                    System.out.println("My number is more ");
+                } else if (myNum < userNum) {
+                    System.out.println("My number is less");
+                } else {
+                    System.out.println("Bingo!");
+                    break;
+                }
             }
-        }
+            System.out.println("You lost!");
+            System.out.println("Do you want to try again?");
+            answer = scanner.next();
+        } while (answer.equals("yes"));
 
+        while (answer != "yes") {
+            System.out.println("Ok! See You next time!");
+            break;
+        }
 
     }
 }

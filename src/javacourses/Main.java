54824b65f80a3a9040b2f82738005375dcabca13
Pauline <dyakovapolina@gmail.com>
2018-02-27 12:44:56 +0200

    Asking question and repeating game (last edits)

--- src/javacourses/Main.java
+++ src/javacourses/Main.java
@@ -9,16 +9,17 @@ public class Main {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         String answer;
-        do {
 
+        do {
             System.out.println("I think of number from 1 to 100. Try to guess it.");
 
             int myNum = random.nextInt(100) + 1;
+            int userNum = 0;
 
             System.out.println(myNum);
             for (int i = 0; i < 10; i++) {
                 System.out.println("My try is " + (i + 1));
-                int userNum = scanner.nextInt();
+                userNum = scanner.nextInt();
 
                 System.out.println(userNum);
 
@@ -31,15 +32,16 @@ public class Main {
                     break;
                 }
             }
-            System.out.println("You lost!");
-            System.out.println("Do you want to try again?");
+            if (myNum == userNum) {
+
+                System.out.println("Do you want to try again?");
+            } else {
+                System.out.println("You lost!");
+                System.out.println("Do you want to try again?");
+            }
             answer = scanner.next();
-        } while (answer.equals("yes"));
 
-        while (answer != "yes") {
-            System.out.println("Ok! See You next time!");
-            break;
-        }
-
+        } while (answer.equals("Yes"));
+        System.out.println("Ok! See You next time!");
     }
 }

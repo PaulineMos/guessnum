966b23aefe28448d945020fa0fa0b3d48cd75a24
Pauline <dyakovapolina@gmail.com>
2018-03-05 21:57:17 +0200

    add exception cmd-d

--- src/javacourses/Main.java
+++ src/javacourses/Main.java
@@ -1,5 +1,6 @@
 package javacourses;
 
+import java.util.NoSuchElementException;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,43 +10,46 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Random random = new Random();
+        try {
+            Random random = new Random();
 
-        String answer;
+            String answer;
 
-        do {
-            System.out.println("I think of number from 1 to 100. Try to guess it.");
+            do {
+                System.out.println("I think of number from 1 to 100. Try to guess it.");
 
-            int myNum = random.nextInt(100) + 1;
-            int userNum = 0;
+                int myNum = random.nextInt(100) + 1;
+                int userNum = 0;
 
-            System.out.println("Spoiler" + myNum);
-            for (int i = 0; i < 10; i++) {
-                System.out.println("My try is " + (i + 1));
-                userNum = askNumber();
+                System.out.println("Spoiler" + myNum);
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("My try is " + (i + 1));
+                    userNum = askNumber();
 
-                System.out.println(userNum);
+                    System.out.println(userNum);
 
-                if (myNum > userNum) {
-                    System.out.println("My number is more ");
-                } else if (myNum < userNum) {
-                    System.out.println("My number is less");
-                } else {
-                    System.out.println("Bingo!");
-                    break;
+                    if (myNum > userNum) {
+                        System.out.println("My number is more ");
+                    } else if (myNum < userNum) {
+                        System.out.println("My number is less");
+                    } else {
+                        System.out.println("Bingo!");
+                        break;
+                    }
                 }
+                if (myNum == userNum) {
+
+                    System.out.println("Do you want to try again?");
+                } else {
+                    System.out.println("You lost!");
+                    System.out.println("Do you want to try again?");
+                }
+                answer = askAnswer();
+
+            } while (answer.equalsIgnoreCase("Yes"));
+        } catch (NoSuchElementException e) {
+            System.out.println("Oh! It's pity! You decided to cancel the game!");
             }
-            if (myNum == userNum) {
-
-                System.out.println("Do you want to try again?");
-            } else {
-                System.out.println("You lost!");
-                System.out.println("Do you want to try again?");
-            }
-            answer = askAnswer();
-
-        } while (answer.equalsIgnoreCase("Yes"));
-
         System.out.println("Ok! See You next time!");
 
     }
